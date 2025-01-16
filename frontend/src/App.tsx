3d53bb06f9e28d9dde3d4deaa995a862b142d548
tonyliu666 <tony6liu@gmail.com>
2025-01-17 00:41:51 +0800

    advancedmarker not work here

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import {APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
+import {Marker,AdvancedMarker,InfoWindow, APIProvider, Map, MapCameraChangedEvent, Pin} from '@vis.gl/react-google-maps';
 import { useState } from "react";
 import Sidebar from "./components/sidebar";
 
 
 const App = () => {
   const [showMap, setShowMap] = useState(false);
+  const [infowindowOpen, setInfowindowOpen] = useState(true);
   // load environment variables
   //const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
   // const apiKey : string = process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string;
@@ -13,24 +14,54 @@ const App = () => {
       <Sidebar setShowMap={setShowMap} />
       <div style={{ flex: 1, padding: "20px" }}>
         {showMap ? (
-        <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+        <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY} libraries={['marker', 'places']}>
           <Map
-             defaultZoom={13}
-             defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
-             onCameraChanged={ (ev: MapCameraChangedEvent) =>
-               console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-             }>
+            defaultZoom={13}
+            defaultCenter={ { lat: 24.7864938, lng: 121.0188275 } }
+            onCameraChanged={ (ev: MapCameraChangedEvent) =>
+              console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
+            }>
+            <InfoWindow position={{lat: 24.7864938, lng: 121.0188275}} maxWidth={200}>
+              <div style={{ color: 'red' }}>
+                <p>The diner</p>
+              </div>
+            </InfoWindow>
+
+            
+
+            {/* <AdvancedMarker
+              position={{lat: 24.7864938, lng: 121.2288275}}
+              title={'AdvancedMarker with custom html content.'}>
+              <div
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  background: '#1dbe80',
+                  border: '2px solid #0e6443',
+                  borderRadius: '50%',
+                  transform: 'translate(-50%, -50%)'
+                }}></div>
+            </AdvancedMarker> */}
+            <Marker
+              position={{lat: 24.7864938, lng: 121.2288275}}
+              clickable={true}
+              onClick={() => alert('marker was clicked!')}
+              title={'clickable google.maps.Marker'}
+            />
+            
+          
+             
           </Map>
         </APIProvider>
         ) : (
           <div>
             <p>Click "Home" to show the content.</p>
-            <p>"API Key: ", {process.env.NODE_ENV}; </p>
-            {/* <p>{process.env.REACT_APP_GOOGLE_MAPS_API_KEY}</p> */}
           </div>
         )}
       </div>
     </div>
+    
   );
 };
 
