da6d0aacc8b1ae3d86d2b9b8ca2accdee648ec74
tonyliu666 <tony6liu@gmail.com>
2025-01-16 22:52:13 +0800

    read the env variables successfully

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,15 +6,14 @@ import Sidebar from "./components/sidebar";
 const App = () => {
   const [showMap, setShowMap] = useState(false);
   // load environment variables
-    const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
-    
+  //const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+  // const apiKey : string = process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string;
   return (
     <div style={{ display: "flex" }}>
       <Sidebar setShowMap={setShowMap} />
       <div style={{ flex: 1, padding: "20px" }}>
         {showMap ? (
-        //   <APIProvider apiKey={apiKey}>
-        <APIProvider apiKey={apiKey}>
+        <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
           <Map
              defaultZoom={13}
              defaultCenter={ { lat: -33.860664, lng: 151.208138 } }
@@ -24,7 +23,11 @@ const App = () => {
           </Map>
         </APIProvider>
         ) : (
-          <p>Click "Home" to show the content.</p>
+          <div>
+            <p>Click "Home" to show the content.</p>
+            <p>"API Key: ", {process.env.NODE_ENV}; </p>
+            {/* <p>{process.env.REACT_APP_GOOGLE_MAPS_API_KEY}</p> */}
+          </div>
         )}
       </div>
     </div>
